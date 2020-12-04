942d192304744919f23b6ed165f94db674842d65
esogelola <esogelola@gmail.com>
2020-12-04 03:44:34 -0500

    Initial Commit

---
+++ src/main/java/com/esogelola/chucknorrisjoke/ChucknorrisJokeApplication.java
@@ -0,0 +1,13 @@
+package com.esogelola.chucknorrisjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorrisJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChucknorrisJokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esogelola/chucknorrisjoke/ChucknorrisJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esogelola.chucknorrisjoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChucknorrisJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
