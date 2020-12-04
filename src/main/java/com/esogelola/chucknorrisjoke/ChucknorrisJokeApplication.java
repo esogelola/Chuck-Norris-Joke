52c1361bff8a86d47110a67e0a284fb53309a9f7
esogelola <esogelola@gmail.com>
2020-12-04 06:17:13 -0500

    Added a XML bean configuration.

--- src/main/java/com/esogelola/chucknorrisjoke/ChucknorrisJokeApplication.java
+++ src/main/java/com/esogelola/chucknorrisjoke/ChucknorrisJokeApplication.java
@@ -2,8 +2,10 @@ package com.esogelola.chucknorrisjoke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChucknorrisJokeApplication {
 
 	public static void main(String[] args) {
